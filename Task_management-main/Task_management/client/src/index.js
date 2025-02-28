f659cfd8b4ee89619c9832a4657c9044def997c5
RAJAT MISHRA <77861069+mst-rajatmishra@users.noreply.github.com>
2025-03-01 01:00:32 +0530

    Delete Task_management-main/Task_management directory

--- Task_management-main/Task_management/client/src/index.js
+++
@@ -1,8 +0,0 @@
-import react from 'react';
-import  ReactDOM  from 'react-dom';
-import App from './App'
-import Loginform from './loginform'
-import createacc from './createacc'
-import {BrowserRouter,Routes,Route} from "react-router-dom";
-
-ReactDOM.render(<App />,document.getElementById('root'))
